a3f3c37f944ebc4c14b35d4d39f9597fc4c5d3f5
patrikduch <duchpatrik@icloud.com>
2023-09-30 20:23:00 +0200

    feat: ASI another example

--- asi/index.js
+++ asi/index.js
@@ -1,3 +1,16 @@
 // ASI - Automatic Semicolon Insertion
 console.log('Patrik Duch')
 console.log('Test')
+
+
+
+function test() {
+    return
+    {
+        name: 'Patrik'
+    };
+}
+
+const result = test();
+console.log(result);
+
